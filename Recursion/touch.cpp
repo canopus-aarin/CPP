5cc3101cbef3d441337505f3a4ca76c35fedc3ff
canopus-aarin <canopus.aarin@gmail.com>
2024-12-20 13:01:44 +0530

    draft commit

--- Recursion/touch.cpp
+++ Recursion/touch.cpp
@@ -1,1 +1,22 @@
-# Hello World!
+#include<iostream>
+using namespace std;
+
+int func(int n)
+{
+    if(n<=1) return n;
+    int last=func(n-1);
+    int slast=func(n-2);
+    return last+slast;
+}
+
+
+
+
+int main()
+{
+    int n;
+    cout<<"Enter some value for n"<<endl;
+    cin>>n;
+    cout<<func(n);
+    return 0;
+}
