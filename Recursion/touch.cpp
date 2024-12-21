9c39eb18f3b5361a159969bfdffed3387a0a5df3
canopus-aarin <canopus.aarin@gmail.com>
2024-12-21 15:04:19 +0530

    Added BinarySearch.cpp (#2)

--- Recursion/touch.cpp
+++ Recursion/touch.cpp
@@ -1,16 +1,14 @@
 #include<iostream>
 using namespace std;
 
-int func(int n)
-{
+
+int func(int n){
     if(n<=1) return n;
+    
     int last=func(n-1);
     int slast=func(n-2);
     return last+slast;
 }
-sdnfdslfdsfsdsdfds
-
-
 
 
 int main()
