3193fd20042d07244a5505b4344299accdf61b12
canopus-aarin <canopus.aarin@gmail.com>
2025-04-15 22:57:03 +0530

    new code

---
+++ ProgramAgain.cpp/PalindNoRec.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+bool isPalindrome(string s){
+
+    int left = 0, right = s.length() - 1;
+    while(left<right){
+
+        if(!isalnum(s[left]))
+        left++;
+        else if(!isalnum(s[right]))
+        right--;
+        else if(tolower(s[left])!= tolower(s[right]))
+        return false;
+
+        else {
+            left++;
+            right--;
+        }
+    }
+    return true;
+}
+
+
+
+int main(){
+
+    string str = "ABCDCBA";
+    bool ans = isPalindrome(str);
+
+    if (ans == true){
+        cout << "Palindrome";
+    }
+    else{
+        cout << "NOT Palindrome";
+    }
+
+    return 0;
+}
